211cb40bdf7bcf3693f97e8504a1300552c0c481
Colin <colinmcneil@gmail.com>
2015-06-10 17:46:52 -0400

    Added comments on code, set debug state to be off.

--- June.py
+++ June.py
@@ -1,13 +1,14 @@
 __author__ = 'Colin'
-import easygui as gui
 __name__='Daily Programs for June'
+import easygui as gui
 
 def June(day):
     if day == '8':
         J8()
 
-
 def J8():
+    #Make any number into a palendrome.
+    #URL: http://www.reddit.com/r/dailyprogrammer/comments/38yy9s/20150608_challenge_218_easy_making_numbers/
     number=gui.enterbox(title=__name__,msg='Input a number and I will make it a palindrome')
     invnumber=int(number[::-1])
     number=int(number)
@@ -19,4 +20,3 @@ def J8():
             gui.textbox(title='Output',text=('It took '+str(i)+' iterations. Final #: '+number))
             break
         i=i+1
-
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+__name__='Daily Program'
 __author__ = 'Colin'
+
 import easygui as gui
 import June
 
-#Debug Line
-debug=1
-June.J8()
+#Debug Line 1 to skip menus, 0 for normal
+debug=0
+
+if debug==1:
+    #Put in specific day to skip menu to in format: Month.DayCode()
+    June.J8()
 
 if debug ==0:
-    __name__='Daily Program'
+    #Menu using easygui to select the day you want to run
     year=gui.choicebox('Pick Year',__name__,['2015'])
     month='Not Defined'
     day='Not Defined'
